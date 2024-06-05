7b8f40b68136321ffc0c25ff8193f7da250966ef
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-06-05 17:11:55 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/index";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    setInterval(() => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          } else {
+            entry.target.classList.remove("show");
+          }
+        });
+      });
+
+      const elements = document.querySelectorAll(".left , .right , .hidden , .top")
+  
+      elements.forEach((el)=>{observer.observe(el)})
+
+      return () => {
+        elements.forEach((el) =>{observer.unobserve(el)})
+      };
+    });
+  }, []);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
